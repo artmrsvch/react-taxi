7aedef4a4a2f68a824d0e231ac2fca41fce38128
Artem_Zabirov <qwertysairex@gmail.com>
2020-01-29 21:58:56 +0300

    install redux and material-ui

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import "./index.scss";
 import App from "./App";
@@ -7,12 +9,16 @@ import * as serviceWorker from "./serviceWorker";
 import { theme } from "loft-taxi-mui-theme";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 
+let store = createStore(reducer);
+
 ReactDOM.render(
-    <BrowserRouter>
-        <MuiThemeProvider theme={theme}>
-            <App />
-        </MuiThemeProvider>
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <MuiThemeProvider theme={theme}>
+                <App />
+            </MuiThemeProvider>
+        </BrowserRouter>
+    </Provider>,
     document.getElementById("root")
 );
 
