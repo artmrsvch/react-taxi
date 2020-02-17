9a92168597fce4f3432c5ca47a13a05d7b297a29
Artem_Zabirov <qwertysairex@gmail.com>
2020-02-18 00:35:39 +0300

    fixed(reload map, added adaptive layout in profile components)

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import mapboxgl from "mapbox-gl";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchRoute, routeReset } from "../../module/actions";
@@ -8,10 +8,8 @@ import CardRequestComplete from "./CardRequestComplete";
 import { drawRoute } from "./drawRoutes";
 import Header from "../Header/Header";
 
-mapboxgl.accessToken =
-    "pk.eyJ1IjoiYXJ0bXJzdmNoIiwiYSI6ImNrNW53YWhiYzBhdGszbW1wdzlndnQ5bHQifQ.4_4_ZfVWfJB2ehd3VRilDA";
-
 function Map() {
+    const [map, setMap] = useState(null);
     const dispatch = useDispatch();
     const { adressList, isCardAdd, selectRoute } = useSelector(state => state);
     let mapContainer;
@@ -26,6 +24,11 @@ function Map() {
     };
     const removeTheWay = () => {
         dispatch(routeReset({ status: false, coords: [] }));
+        const layers = ["route", "start", "start-inner", "finish", "finish-inner"];
+        layers.forEach(layer => {
+            map.removeLayer(layer);
+        });
+        map.removeSource("route");
     };
     const renderForms = () => {
         if (selectRoute.status) return <CardRequestComplete submit={removeTheWay} />;
@@ -36,19 +39,19 @@ function Map() {
         );
     };
     useEffect(() => {
-        const map = new mapboxgl.Map({
+        selectRoute.status && drawRoute(map, selectRoute.coords);
+    }, [map, selectRoute]);
+    useEffect(() => {
+        mapboxgl.accessToken =
+            "pk.eyJ1IjoiYXJ0bXJzdmNoIiwiYSI6ImNrNW53YWhiYzBhdGszbW1wdzlndnQ5bHQifQ.4_4_ZfVWfJB2ehd3VRilDA";
+        const mapbox = new mapboxgl.Map({
             container: mapContainer,
             style: "mapbox://styles/mapbox/dark-v9",
             center: [option.lng, option.lat],
             zoom: option.zoom
         });
-        map.on("load", () => {
-            selectRoute.status && drawRoute(map, selectRoute.coords);
-        });
-        return () => {
-            map.remove();
-        };
-    }, [option]);
+        setMap(mapbox);
+    }, []);
 
     return (
         <>
